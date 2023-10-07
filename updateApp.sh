1ef9397e2e69b7a94a03bd34b38eadbf9a47e024
Gilad Bracha <gilad@bracha.org>
2023-10-06 17:39:05 -0700

    Reanme PWA to webIDE. Rename electron app to avoid clash with PWA.  Fix some comments/typos.

--- updateApp.sh
+++ updateApp.sh
@@ -4,7 +4,7 @@ source build.sh
 # If on a mac, leave the definition of APPVFUELPATH untouched.
 #  If one Windows, if your app is located at x, then set
 # APPVFUELPATH=x/resources/app/out/
-APPVFUELPATH=/Applications/NewspeakIDE.app/Contents/Resources/app/out/
+APPVFUELPATH=/Applications/NewspeakElectronIDE.app/Contents/Resources/app/out/
 cp out/HopscotchElectronIDE.vfuel $APPVFUELPATH
 cp out/primordialsoup.wasm $APPVFUELPATH
 cp out/primordialsoup.js $APPVFUELPATH

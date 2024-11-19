1ac0ee38bee3454385720f9f43bcb2a377e540ec
Gilad Bracha <gilad@bracha.org>
2024-11-19 15:47:39 -0800

    Sync of file loading works.

--- platforms/webIDE/sw.js
+++ platforms/webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 428;
+const pwaVersion = 438;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;

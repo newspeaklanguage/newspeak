a2e20fda4bebdac66760316d028f1efb8ed7c241
Gilad Bracha <gilad@bracha.org>
2025-11-20 20:18:55 -0800

    Remove Electron from the system. See previous commit on build script for reasons.

--- platforms/webIDE/sw.js
+++ platforms/webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 811;
+const pwaVersion = 816;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;

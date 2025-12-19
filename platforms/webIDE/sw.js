ecc7ea30065cd08914294e050bbcb89d78455e9a
Gilad Bracha <gilad@bracha.org>
2025-12-19 15:53:41 -0800

    WIP on voice control. Fix so classes (or anything that starts with uppercase) shows up.

--- platforms/webIDE/sw.js
+++ platforms/webIDE/sw.js
@@ -65,7 +65,7 @@
 // The version update can be done either manually (we rely on this),
 // or by applying some framework that generates this sw.js worker script.
 
-const pwaVersion = 855;
+const pwaVersion = 856;
 
 // Cache name used by the 'caches' global variable.
 // The pwaCacheName now includes the pwaVersion;
